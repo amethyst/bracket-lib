99c01d6d8a0358d4ffb7a2d157dcfc5789bd1fe0
Herbert Wolverson <herberticus@gmail.com>
2019-07-11 11:52:25 -0500

    Example 9 is somewhat work-in-progress, but implements offsets to console layers, as requested by megane / ontoclasm on the Roguelike discord.

--- README.md
+++ README.md
@@ -110,3 +110,11 @@ Run this example with `cargo run --example ex07-tiles` from the root of the clon
 GridSageGames' excellent [REX Paint](https://www.gridsagegames.com/rexpaint/)), and displaying it on the console. Thanks to Mara "Alkahest"'s hard work (see original repo [here](https://gitlab.com/medusacle/rs-rexpaint)), you can load and save `.xp` files as you need them.
 
 Run this example with `cargo run --example ex08-rex` from the root of the cloned repository.
+
+### Example 9: Layer Offsets
+
+![Animated GIF](/screenshots/RLTK_RS_EXAMPLE09.gif)
+
+[Example 9 - Offsets](examples/ex09-offsets.rs) demonstrates the ability to call `set_offset` on a console layer, to nudge it in a direction. In this case, it is used to render thin walls *between* the major tiles.
+
+Run this example with `cargo run --example ex09-offsets` from the root of the cloned repository.
---
+++ examples/ex09-offsets.rs
@@ -0,0 +1,46 @@
+extern crate rltk;
+
+use rltk::{Rltk, GameState, Console, RGB, VirtualKeyCode, to_cp437};
+
+struct State {
+}
+
+impl GameState for State {
+
+    fn tick(&mut self, ctx : &mut Rltk) {
+        ctx.set_active_console(0);
+        ctx.cls();
+        ctx.print_color_centered(3, RGB::named(rltk::WHITE), RGB::named(rltk::BLACK), "Offset Consoles");
+        ctx.print_color_centered(4, RGB::named(rltk::GRAY), RGB::named(rltk::BLACK), "For those times you want thin walls");
+
+        for y in 20..30 {
+            for x in 30..50 {                
+                ctx.set(x, y, RGB::named(rltk::GRAY), RGB::named(rltk::BLACK), to_cp437('░'))
+            }
+        }
+
+        ctx.set(40, 25, RGB::named(rltk::YELLOW), RGB::named(rltk::BLACK), to_cp437('☺'));
+
+        ctx.set_active_console(1);
+        ctx.cls();
+        ctx.print_color(38, 24, RGB::named(rltk::WHITE), RGB::named(rltk::BLACK), "┌──┐");
+        ctx.print_color(38, 25, RGB::named(rltk::WHITE), RGB::named(rltk::BLACK), "│  │");
+        ctx.print_color(38, 26, RGB::named(rltk::WHITE), RGB::named(rltk::BLACK), "│  │");
+        ctx.print_color(38, 27, RGB::named(rltk::WHITE), RGB::named(rltk::BLACK), "└┐ │");
+        ctx.print_color(38, 28, RGB::named(rltk::WHITE), RGB::named(rltk::BLACK), " └─┘");
+    }
+}
+
+fn main() {
+    let mut context = Rltk::init_simple8x8(80, 50, "Example 9 - Offset Tiles", "resources");
+    context.register_console_no_bg(rltk::SparseConsole::init(80, 50, &context.gl), 0);
+
+    // We're going to set the second layer's offset to -0.5 to render between tiles
+    context.set_active_console(1);
+    context.set_offset(0.5, 0.5);
+
+    // Returning to the default console is a good plan
+    context.set_active_console(0);
+    let gs : State = State{ };
+    rltk::main_loop(context, Box::new(gs));
+}
