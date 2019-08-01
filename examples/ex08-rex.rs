bb7f06e4b98cde474c520c12d19d525b129437d9
Herbert Wolverson <herberticus@gmail.com>
2019-08-01 08:31:31 -0500

    Clippy - example 8

--- examples/ex08-rex.rs
+++ examples/ex08-rex.rs
@@ -37,14 +37,11 @@ impl GameState for State {
             None => {} // Nothing happened
             Some(key) => {
                 // A key is pressed or held
-                match key {
-                    VirtualKeyCode::S => {
-                        // Demonstrates saving the console stack on an xp file
-                        let xpfile = ctx.to_xp_file(80, 50);
-                        let mut f = File::create("./screenshot.xp").expect("Unable to create file");
-                        xpfile.write(&mut f).expect("Unable to save file");
-                    }
-                    _ => {}
+                if let VirtualKeyCode::S = key {
+                    // Demonstrates saving the console stack on an xp file
+                    let xpfile = ctx.to_xp_file(80, 50);
+                    let mut f = File::create("./screenshot.xp").expect("Unable to create file");
+                    xpfile.write(&mut f).expect("Unable to save file");
                 }
             }
         }
