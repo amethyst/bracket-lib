6d6cf58854e1ab37ba6ec7c9a3bd29ec56a023b1
Herbert Wolverson <herberticus@gmail.com>
2019-10-17 13:04:48 -0500

    Move to a platform-agnostic outer log, HAL exposes types.

--- src/console.rs
+++ src/console.rs
@@ -1,4 +1,4 @@
-use super::{rex::XpLayer, Font, Shader, RGB};
+use super::{rex::XpLayer, Font, Shader, RGB, hal};
 
 #[cfg(target_arch = "wasm32")]
 use wasm_bindgen::prelude::*;
@@ -92,14 +92,6 @@ pub trait Console {
     fn set_offset(&mut self, x: f32, y: f32);
 }
 
-#[cfg(not(target_arch = "wasm32"))]
 pub fn log<S:ToString>(message: S) {
-    println!("{}", message.to_string());
-}
-
-#[cfg(target_arch = "wasm32")]
-#[wasm_bindgen]
-extern "C" {
-    #[wasm_bindgen(js_namespace = console)]
-    pub fn log(s: &str);
+    hal::log(message);
 }
--- src/hal/native/mod.rs
+++ src/hal/native/mod.rs
@@ -17,3 +17,15 @@ pub struct WrappedContext {
     pub el: glutin::event_loop::EventLoop<()>,
     pub wc: glutin::WindowedContext<glutin::PossiblyCurrent>,
 }
+
+#[cfg(not(target_arch = "wasm32"))]
+pub fn log<S:ToString>(message: S) {
+    println!("{}", message.to_string());
+}
+
+#[cfg(target_arch = "wasm32")]
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    pub fn log(s: &str);
+}
