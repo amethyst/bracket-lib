fee5f3822c52da7e95babb3f5a44962ce190a01a
Herbert Wolverson <herberticus@gmail.com>
2019-11-19 12:00:58 -0600

    Second try on Mac support. Stick to GL2, fix the framebuffer format.

--- src/hal/font.rs
+++ src/hal/font.rs
@@ -128,12 +128,12 @@ impl Font {
             gl.tex_parameter_i32(
                 glow::TEXTURE_2D,
                 glow::TEXTURE_MIN_FILTER,
-                glow::LINEAR as i32,
+                glow::NEAREST as i32,
             );
             gl.tex_parameter_i32(
                 glow::TEXTURE_2D,
                 glow::TEXTURE_MAG_FILTER,
-                glow::LINEAR as i32,
+                glow::NEAREST as i32,
             );
 
             let img_orig = Font::load_image(&self.bitmap_file);
