ed1cd517ddf19af842f370f282e00e33e8243fb9
Herbert Wolverson <herberticus@gmail.com>
2019-11-22 16:31:09 -0600

    Fixed the need for RON files to open window, still pondering getting RLTK data into Amethyst.

--- src/hal/amethyst_be/mod.rs
+++ src/hal/amethyst_be/mod.rs
@@ -10,6 +10,13 @@ use amethyst::{
     },
     utils::application_root_dir,
 };
+use amethyst::{
+    assets::{AssetStorage, Loader, Handle},
+    core::transform::Transform,
+    ecs::prelude::{Component, DenseVecStorage},
+    prelude::*,
+    renderer::{Camera, ImageFormat, SpriteRender, SpriteSheet, SpriteSheetFormat, Texture},
+};
 
 mod keycodes;
 pub use keycodes::VirtualKeyCode;
@@ -71,35 +78,44 @@ pub fn init_raw<S: ToString>(
 }
 
 pub struct RltkGemBridge {
-    rltk : Rltk,
-    gamestate : Box<dyn GameState>
 }
 
 impl SimpleState for RltkGemBridge {
     fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
-        println!("Start called");
+        let world = data.world;
+        let mut transform = Transform::default();
+        transform.set_translation_xyz(640.0 * 0.5, 400.0 * 0.5, 1.0);
+
+        world
+            .create_entity()
+            .with(Camera::standard_2d(640.0, 400.0))
+            .with(transform)
+            .build();
     }
 }
 
 pub fn main_loop<GS: GameState>(mut rltk: Rltk, mut gamestate: GS) {
     amethyst::start_logger(Default::default());
 
+    let mut cfg = amethyst::window::DisplayConfig::default();
+    cfg.dimensions = Some((640, 400));
+    cfg.title = "Hello RLTK".to_string();
+
     let app_root = application_root_dir().unwrap();
-    let display_config_path = app_root.join("config").join("display.ron");
     let game_data = GameDataBuilder::default()
         .with_bundle(
             RenderingBundle::<DefaultBackend>::new()
             // The RenderToWindow plugin provides all the scaffolding for opening a window and drawing on it
             .with_plugin(
-                RenderToWindow::from_config_path(display_config_path)
+                RenderToWindow::from_config(cfg)
                     .with_clear([0.00196, 0.23726, 0.21765, 1.0]),
             )
             // RenderFlat2D plugin is used to render entities with a `SpriteRender` component.
             .with_plugin(RenderFlat2D::default()),
         ).unwrap();
     let assets_dir = app_root.join("assets");
-    let mut world = World::new();
-    let mut game = Application::new(assets_dir, RltkGemBridge{rltk, gamestate: Box::new(gamestate)}, game_data).unwrap();
+    //let mut world = World::new(); // Why is this even here?
+    let mut game = Application::new(assets_dir, RltkGemBridge{}, game_data).unwrap();
     game.run();
 }
 
