a79aa908ccabfca139d117e2c6c74dedb50453e0
Herbert Wolverson <herberticus@gmail.com>
2019-11-22 09:03:27 -0600

    Cleanup pancurses support layout.

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,7 @@ mod sparse_console;
 pub mod textblock;
 pub use hal::*;
 
-pub use self::codepage437::{string_to_cp437, to_cp437};
+pub use self::codepage437::{string_to_cp437, to_cp437, to_char};
 pub use self::color::*;
 pub use self::console::*;
 pub use self::fastnoise::*;
