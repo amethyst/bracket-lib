d761a172903d9ba1cf610ce0083e08ea00189f96
Herbert Wolverson <herberticus@gmail.com>
2019-11-23 11:31:22 -0600

    Fixed the need for RON files to open window, still pondering getting RLTK data into Amethyst.

--- src/hal/amethyst_be/mod.rs
+++ src/hal/amethyst_be/mod.rs
@@ -9,8 +9,6 @@ use amethyst::{
         RenderingBundle,
     },
     utils::application_root_dir,
-};
-use amethyst::{
     assets::{AssetStorage, Loader, Handle},
     core::transform::Transform,
     ecs::prelude::{Component, DenseVecStorage},
@@ -28,14 +26,24 @@ pub mod shader {
 }
 
 pub mod font {
+    use amethyst::{
+        renderer::SpriteSheet,
+        renderer::Texture,
+        assets::Handle
+    };
+
     pub struct Font{
-        pub tile_size: (u32, u32)
+        pub tile_size: (u32, u32),
+        pub filename : String,
+        pub ss : Option<Handle<SpriteSheet>>
     }
 
     impl Font {
-        pub fn load<S: ToString>(_filename: S, _tile_size: (u32, u32)) -> Font {
+        pub fn load<S: ToString>(filename: S, tile_size: (u32, u32)) -> Font {
             Font{
-                tile_size : (0, 0)
+                tile_size,
+                filename : filename.to_string(),
+                ss : None
             }
         }
 
@@ -78,27 +86,181 @@ pub fn init_raw<S: ToString>(
 }
 
 pub struct RltkGemBridge {
+    rltk : Rltk,
+    state : Box<dyn GameState>
 }
 
 impl SimpleState for RltkGemBridge {
     fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
         let world = data.world;
+        world.register::<SimpleConsoleComponent>();
+        self.make_camera(world);
+        let ss = self.initialize_fonts(world);
+        self.initialize_console_objects(world, ss);
+    }
+
+    /*fn update(&mut self, _data: &mut StateData<'_, GameData<'_, '_>>) -> amethyst::SimpleTrans {
+        // Handle Input Somehow
+
+        // Tick the game's state
+        self.state.tick(&mut self.rltk);
+
+        // Quit if RLTK wants to (it's my party and I'll quit if I want to)
+        if self.rltk.quitting {
+            return Trans::Quit;
+        }
+
+        // Update the console state objects
+
+        Trans::None
+    }*/
+}
+
+pub const ARENA_HEIGHT: f32 = 100.0;
+pub const ARENA_WIDTH: f32 = 100.0;
+pub const PADDLE_HEIGHT: f32 = 16.0;
+pub const PADDLE_WIDTH: f32 = 4.0;
+
+impl RltkGemBridge {
+    fn make_camera(&self, world : &mut World) {
         let mut transform = Transform::default();
-        transform.set_translation_xyz(640.0 * 0.5, 400.0 * 0.5, 1.0);
+        let width = self.rltk.width_pixels as f32;
+        let height = self.rltk.height_pixels as f32;
+        println!("{} x {}", width, height);
+        transform.set_translation_xyz(ARENA_WIDTH * 0.5, ARENA_HEIGHT * 0.5, 1.0);
 
         world
             .create_entity()
-            .with(Camera::standard_2d(640.0, 400.0))
+            .with(Camera::standard_2d(ARENA_WIDTH, ARENA_HEIGHT))
             .with(transform)
             .build();
     }
+
+    fn initialize_fonts(&mut self, world : &mut World) -> Handle<SpriteSheet> {
+        use amethyst::renderer::Sprite;
+
+        let loader = world.read_resource::<Loader>();
+        let texture_storage = world.read_resource::<AssetStorage<Texture>>();
+        let ss_storage = world.read_resource::<AssetStorage<SpriteSheet>>();
+
+        let handle = loader.load(
+            "resources/pong_spritesheet.png",
+            ImageFormat::default(),
+            (),
+            &texture_storage
+        );
+        loader.load(
+            "resources/test.ron",
+            SpriteSheetFormat(handle),
+            (),
+            &ss_storage
+        )
+
+        /*
+        for font in self.rltk.fonts.iter_mut() {
+            let handle = loader.load(
+                &font.filename,
+                ImageFormat::default(),
+                (),
+                &texture_storage
+            );
+            
+            // Make a font-specific sprite sheet
+            /*
+            let offsets = [0.5; 2];
+            let mut sprites = Vec::with_capacity(256);
+
+            for y in 0..16 {
+                for x in 0..16 {
+                    let sprite = Sprite::from_pixel_values(
+                        font.tile_size.0 * 16,
+                        font.tile_size.1 * 16,
+                        font.tile_size.0,
+                        font.tile_size.1,
+                        x * font.tile_size.0,
+                        y * font.tile_size.1,
+                        offsets,
+                        false,
+                        false
+                    );
+                    println!("{:?}", sprite);
+                    sprites.push(sprite);
+                }
+            }*/
+
+            /*
+            sprites.push(Sprite::from_pixel_values(
+                128, 128, 128, 128, 0, 0, offsets, false, false
+            ));
+            let ss_handle = loader.load_from_data(
+                SpriteSheet{ texture: handle.clone(), sprites },
+                (),
+                &ss_storage
+            );
+            font.ss = Some(ss_handle);
+            println!("{:?}", font.ss);*/
+
+            font.ss = Some(loader.load(
+                "resources/test.ron",
+                SpriteSheetFormat(handle),
+                (),
+                &ss_storage
+            ));
+            println!("Loaded font");
+        }*/
+    }
+
+    fn initialize_console_objects(&self, world : &mut World, ss : Handle<SpriteSheet>) {
+        let mut transform = Transform::default();
+        let y = ARENA_HEIGHT / 2.0;
+        transform.set_translation_xyz(PADDLE_WIDTH * 0.5, y, 0.0);
+
+        let sprites = SpriteRender{
+            sprite_sheet: ss.clone(),
+            sprite_number: 0
+        };
+
+        world
+            .create_entity()
+            .with(transform.clone())
+            .with(sprites.clone())
+            .build();
+        
+        println!("Made console");
+
+        /*for (i,cons) in self.rltk.consoles.iter().enumerate() {
+            if let Some(ss) = &self.rltk.fonts[cons.font_index].ss {
+                let sprites = SpriteRender{
+                    sprite_sheet: ss.clone(),
+                    sprite_number: 0
+                };
+
+                world
+                    .create_entity()
+                    .with(SimpleConsoleComponent{ idx : i})
+                    .with(transform.clone())
+                    .with(sprites.clone())
+                    .build();
+                
+                    println!("Made console");
+            }
+        }*/
+    }
 }
 
-pub fn main_loop<GS: GameState>(mut rltk: Rltk, mut gamestate: GS) {
+struct SimpleConsoleComponent {
+    idx : usize
+}
+
+impl Component for SimpleConsoleComponent {
+    type Storage = DenseVecStorage<Self>;
+}
+
+pub fn main_loop<GS: GameState>(rltk: Rltk, gamestate: GS) {
     amethyst::start_logger(Default::default());
 
     let mut cfg = amethyst::window::DisplayConfig::default();
-    cfg.dimensions = Some((640, 400));
+    cfg.dimensions = Some((500, 500));
     cfg.title = "Hello RLTK".to_string();
 
     let app_root = application_root_dir().unwrap();
@@ -107,15 +269,16 @@ pub fn main_loop<GS: GameState>(mut rltk: Rltk, mut gamestate: GS) {
             RenderingBundle::<DefaultBackend>::new()
             // The RenderToWindow plugin provides all the scaffolding for opening a window and drawing on it
             .with_plugin(
-                RenderToWindow::from_config(cfg)
-                    .with_clear([0.00196, 0.23726, 0.21765, 1.0]),
+                RenderToWindow::from_config_path("config/")
+                //RenderToWindow::from_config(cfg)
+                    //.with_clear([0.00196, 0.23726, 0.21765, 1.0]),
             )
             // RenderFlat2D plugin is used to render entities with a `SpriteRender` component.
             .with_plugin(RenderFlat2D::default()),
         ).unwrap();
-    let assets_dir = app_root.join("assets");
+    let assets_dir = app_root;
     //let mut world = World::new(); // Why is this even here?
-    let mut game = Application::new(assets_dir, RltkGemBridge{}, game_data).unwrap();
+    let mut game = Application::new(assets_dir, RltkGemBridge{rltk, state: Box::new(gamestate)}, game_data).unwrap();
     game.run();
 }
 
