7306dfe9467b30fbebb0c84abbd5c787794a7bec
Herbert Wolverson <herberticus@gmail.com>
2019-11-27 15:33:22 -0600

    Update to allow embedded image resources.

--- src/hal/amethyst_be/mainloop.rs
+++ src/hal/amethyst_be/mainloop.rs
@@ -161,21 +161,44 @@ impl RltkGemBridge {
 
     fn initialize_fonts(&mut self, world : &mut World) {
         use amethyst::renderer::Sprite;
-        use std::path::*;
+        use amethyst::renderer::types::{TextureData};
+        use amethyst::renderer::rendy::texture::TextureBuilder;
+        use crate::embedding;
+        use amethyst::renderer::Format;
 
         let loader = world.read_resource::<Loader>();
         let texture_storage = world.read_resource::<AssetStorage<Texture>>();
         let ss_storage = world.read_resource::<AssetStorage<SpriteSheet>>();
         let app_root = application_root_dir().expect("Fail");
+        use image::GenericImageView;
+        use amethyst::renderer::rendy::*;
 
         for font in self.rltk.fonts.iter_mut() {
-            let filename = app_root.join(font.filename.clone());
-            let handle = loader.load(
-                filename.to_str().unwrap(),
-                ImageFormat::default(),
-                (),
-                &texture_storage
-            );
+            let resource = embedding::EMBED
+                .lock()
+                .unwrap()
+                .get_resource(font.filename.to_string());
+            
+                let handle;
+            if let Some(data) = resource {
+                let png = image::load_from_memory(data).expect("Failed to load texture from memory");
+                let texture_builder = TextureBuilder::new()
+                    .with_data_width(png.width())
+                    .with_data_height(png.height())
+                    .with_kind(hal::image::Kind::D2(png.width(), png.height(), 1, 1))
+                    .with_view_kind(hal::image::ViewKind::D2)
+                    .with_sampler_info(hal::image::SamplerInfo::new(hal::image::Filter::Linear, hal::image::WrapMode::Clamp))
+                    .with_raw_data(png.raw_pixels(), Format::Rgba8Unorm);
+                handle = loader.load_from_data(TextureData(texture_builder), (), &texture_storage);
+            } else {
+                let filename = app_root.join(font.filename.clone());
+                handle = loader.load(
+                    filename.to_str().unwrap(),
+                    ImageFormat::default(),
+                    (),
+                    &texture_storage
+                );
+            }
             
             // Make a font-specific sprite sheet            
             let offsets = [0.0 - (font.tile_size.0 as f32 / 2.0), 0.0 - (font.tile_size.1 as f32 / 2.0)];
