9191f535fc6fcc91c4e8c0217b653caed13df3e2
Herbert Wolverson <herberticus@gmail.com>
2020-01-21 10:19:56 -0600

    #48 - Use try_into rather than as for type adjustment, to remove a problem with the underlying type varying in color_pairs (curses). See https://github.com/thebracket/rltk_rs/pull/49 for the original.

--- src/hal/curses/simple_console_backing.rs
+++ src/hal/curses/simple_console_backing.rs
@@ -2,6 +2,7 @@ use super::super::RltkPlatform;
 use super::find_nearest_color;
 use super::font;
 use super::shader;
+use std::convert::TryInto;
 
 pub struct SimpleConsoleBackend {
     tiles: Vec<crate::Tile>,
@@ -43,7 +44,7 @@ impl SimpleConsoleBackend {
                 let cp_fg = find_nearest_color(t.fg, &platform.platform.color_map);
                 let cp_bg = find_nearest_color(t.bg, &platform.platform.color_map);
                 let pair = (cp_bg * 16) + cp_fg;
-                window.attrset(pancurses::COLOR_PAIR(pair as u64));
+                window.attrset(pancurses::COLOR_PAIR(pair.try_into().unwrap()));
                 window.mvaddch(
                     height as i32 - (y as i32 + 1),
                     x as i32,
--- src/hal/curses/sparse_console_backing.rs
+++ src/hal/curses/sparse_console_backing.rs
@@ -2,6 +2,7 @@ use super::super::RltkPlatform;
 use super::find_nearest_color;
 use super::font;
 use super::shader;
+use std::convert::TryInto;
 
 pub struct SparseConsoleBackend {
     width: u32,
@@ -44,7 +45,7 @@ impl SparseConsoleBackend {
             let cp_fg = find_nearest_color(t.fg, &platform.platform.color_map);
             let cp_bg = find_nearest_color(t.bg, &platform.platform.color_map);
             let pair = (cp_bg * 16) + cp_fg;
-            window.attrset(pancurses::COLOR_PAIR(pair as u64));
+            window.attrset(pancurses::COLOR_PAIR(pair.try_into().unwrap()));
             window.mvaddch(
                 self.height as i32 - (y as i32 + 1),
                 x as i32,
