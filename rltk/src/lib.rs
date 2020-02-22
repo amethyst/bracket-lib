6b94201ef7a911ba3587500818e8188cbc6560a0
Herbert Wolverson <herberticus@gmail.com>
2020-02-22 11:18:56 -0600

    RLTK now successfully wraps for use in the roguelike tutorial without having to make tutorial changes.

--- rltk/src/lib.rs
+++ rltk/src/lib.rs
@@ -3,6 +3,17 @@ pub use bracket_lib::prelude::BTerm as Rltk;
 pub use bracket_lib::prelude::BTermBuilder as RltkBuilder;
 pub use bracket_lib::prelude::rex;
 
+#[macro_export]
+macro_rules! link_resource {
+    ($resource_name : ident, $filename : expr) => {
+        rltk::EMBED
+            .lock()
+            .unwrap()
+            .add_resource($filename.to_string(), $resource_name);
+    };
+}
+
 pub mod prelude {
     pub use crate::*;
+    pub use crate::link_resource;
 }
