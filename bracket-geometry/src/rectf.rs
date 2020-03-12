a8d9e4dbe46aa6096884b86d40225b71b9dfea01
Herbert Wolverson <herberticus@gmail.com>
2020-03-12 09:28:59 -0500

    More infrastructure ready for sprite layer.

---
+++ bracket-geometry/src/rectf.rs
@@ -0,0 +1,101 @@
+use crate::prelude::PointF;
+use std::convert::TryInto;
+use std::ops;
+
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[derive(PartialEq, Copy, Clone, Debug)]
+pub struct RectF {
+    pub x1: f32,
+    pub x2: f32,
+    pub y1: f32,
+    pub y2: f32,
+}
+
+#[cfg(feature = "specs")]
+impl specs::prelude::Component for RectF {
+    type Storage = specs::prelude::VecStorage<Self>;
+}
+
+impl Default for RectF {
+    fn default() -> RectF {
+        RectF::zero()
+    }
+}
+
+impl RectF {
+    // Create a new rectangle, specifying X/Y Width/Height
+    pub fn with_size<T>(x: T, y: T, w: T, h: T) -> RectF
+    where
+        T: TryInto<f32>,
+    {
+        let x_f32: f32 = x.try_into().ok().unwrap();
+        let y_f32: f32 = y.try_into().ok().unwrap();
+        RectF {
+            x1: x_f32,
+            y1: y_f32,
+            x2: x_f32 + w.try_into().ok().unwrap(),
+            y2: y_f32 + h.try_into().ok().unwrap(),
+        }
+    }
+
+    // Create a new rectangle, specifying exact dimensions
+    pub fn with_exact<T>(x1: T, y1: T, x2: T, y2: T) -> RectF
+    where
+        T: TryInto<f32>,
+    {
+        RectF {
+            x1: x1.try_into().ok().unwrap(),
+            y1: y1.try_into().ok().unwrap(),
+            x2: x2.try_into().ok().unwrap(),
+            y2: y2.try_into().ok().unwrap(),
+        }
+    }
+
+    // Creates a zero rectangle
+    pub fn zero() -> RectF {
+        RectF {
+            x1: 0.0,
+            y1: 0.0,
+            x2: 0.0,
+            y2: 0.0,
+        }
+    }
+
+    // Returns true if this overlaps with other
+    pub fn intersect(&self, other: &RectF) -> bool {
+        self.x1 <= other.x2 && self.x2 >= other.x1 && self.y1 <= other.y2 && self.y2 >= other.y1
+    }
+
+    // Returns the center of the rectangle
+    pub fn center(&self) -> PointF {
+        PointF{x:(self.x1 + self.x2) / 2.0, y:(self.y1 + self.y2) / 2.0}
+    }
+
+    // Returns true if a point is inside the rectangle
+    pub fn point_in_rect(&self, point: PointF) -> bool {
+        point.x >= self.x1 && point.x <= self.x2 && point.y >= self.y1 && point.y <= self.y2
+    }
+
+    // Returns the rectangle's width
+    pub fn width(&self) -> f32 {
+        f32::abs(self.x2 - self.x1)
+    }
+
+    // Returns the rectangle's height
+    pub fn height(&self) -> f32 {
+        f32::abs(self.y2 - self.y1)
+    }
+}
+
+impl ops::Add<RectF> for RectF {
+    type Output = RectF;
+    fn add(mut self, rhs: RectF) -> RectF {
+        let w = self.width();
+        let h = self.height();
+        self.x1 += rhs.x1;
+        self.x2 = self.x1 + w;
+        self.y1 += rhs.y1;
+        self.y2 = self.y1 + h;
+        self
+    }
+}
--- bracket-terminal/examples/sprites.rs
+++ bracket-terminal/examples/sprites.rs
@@ -3,68 +3,37 @@ bracket_terminal::add_wasm_support!();
 use bracket_terminal::prelude::*;
 
 struct State {
-    x: f32,
-    spin: f32,
 }
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
-        let mut draw_batch = DrawBatch::new();
-
-        // Show frame rate
-        draw_batch.target(1);
-        draw_batch.cls();
-
-        draw_batch.draw_double_box(
-            Rect::with_size(39, 0, 20, 3),
-            ColorPair::new(RGB::named(WHITE), RGB::named(BLACK)),
-        );
-        draw_batch.print_color(
-            Point::new(40, 1),
-            &format!("FPS: {}", ctx.fps),
-            ColorPair::new(RGB::named(YELLOW), RGB::named(BLACK)),
-        );
-        draw_batch.print_color(
-            Point::new(40, 2),
-            &format!("Frame Time: {} ms", ctx.frame_time_ms),
-            ColorPair::new(RGB::named(CYAN), RGB::named(BLACK)),
-        );
-
-        let y = 20.0 + (f32::sin(self.x / 2.0) * 5.0);
-        let scale = f32::cos(self.x / 3.0) + 1.1;
-        draw_batch.set_fancy(
-            PointF { x: self.x, y },
-            0,
-            self.spin,
-            PointF { x: scale, y: scale },
-            ColorPair::new(RGB::named(YELLOW), RGB::named(BLACK)),
-            to_cp437('@'),
-        );
-
-        // Submission
-        draw_batch.submit(0).expect("Batch error");
-        render_draw_buffer(ctx).expect("Render error");
-
-        // Moving
-        self.x += 0.1;
-        if self.x > 80.0 {
-            self.x = 0.0;
-        }
-        self.spin += 0.2;
+        ctx.cls();
+        ctx.print(1,1, "Watch him go!");
     }
 }
 
+bracket_terminal::embedded_resource!(NYAN_CAT, "../resources/sprite_dood.png");
+
 fn main() -> BError {
+    bracket_terminal::link_resource!(NYAN_CAT, "../resources/sprite_dood.png");
+
     let mut context = BTermBuilder::simple80x50()
         .with_sprite_console(640, 400)
         .with_title("Bracket Terminal - Sprite Console")
+        .with_spritesheet(
+            SpriteSheet::new("sprite_dood.png")
+            .add_sprite(Rect::with_size(0, 0, 83, 105))
+            .add_sprite(Rect::with_size(83, 0, 83, 105))
+            .add_sprite(Rect::with_size(166, 0, 83, 105))
+            .add_sprite(Rect::with_size(249, 0, 83, 105))
+        )
         .with_vsync(false)
         .build()?;
 
     context.with_post_scanlines(true);
     context.with_post_scanlines(true);
 
-    let gs = State { x: 0.0, spin: 0.0 };
+    let gs = State {  };
 
     main_loop(context, gs)
 }
