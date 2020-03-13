1ebb867398aa276cfd8e1f51077b23267da3a677
Herbert Wolverson <herberticus@gmail.com>
2020-03-13 15:49:55 -0500

    restore all examples to webglbuild.

--- bracket-terminal/webglbuild.bat
+++ bracket-terminal/webglbuild.bat
@@ -1,24 +1,24 @@
 @ECHO OFF
 REM Build the actual WASM files and helpers
-REM CALL :StageExample hello_minimal, hello_minimal
-REM CALL :StageExample hello_terminal, hello_terminal
-REM CALL :StageExample astar_mouse, astar_mouse
-REM CALL :StageExample dwarfmap, dwarfmap
-REM CALL :StageExample postprocess, postprocess
-REM CALL :StageExample rex, rex
-REM CALL :StageExample sparse, sparse
-REM CALL :StageExample textblock, textblock
-REM CALL :StageExample textsprites, textsprites
-REM CALL :StageExample tiles, tiles
-REM CALL :StageExample walking, walking
-REM CALL :StageExample no_cls, no_cls
-REM CALL :StageExample native_gl, native_gl
-REM CALL :StageExample keyboard, keyboard
-REM CALL :StageExample input_harness, input_harness
-REM CALL :StageExample virtual_console, virtual_console
-REM CALL :StageExample alpha, alpha
-REM CALL :StageExample colorfont, colorfont
-REM CALL :StageExample fontswitch, fontswitch
+CALL :StageExample hello_minimal, hello_minimal
+CALL :StageExample hello_terminal, hello_terminal
+CALL :StageExample astar_mouse, astar_mouse
+CALL :StageExample dwarfmap, dwarfmap
+CALL :StageExample postprocess, postprocess
+CALL :StageExample rex, rex
+CALL :StageExample sparse, sparse
+CALL :StageExample textblock, textblock
+CALL :StageExample textsprites, textsprites
+CALL :StageExample tiles, tiles
+CALL :StageExample walking, walking
+CALL :StageExample no_cls, no_cls
+CALL :StageExample native_gl, native_gl
+CALL :StageExample keyboard, keyboard
+CALL :StageExample input_harness, input_harness
+CALL :StageExample virtual_console, virtual_console
+CALL :StageExample alpha, alpha
+CALL :StageExample colorfont, colorfont
+CALL :StageExample fontswitch, fontswitch
 CALL :StageExample fancy, fancy
 CALL :StageExample sprites, sprites
 
