c5cd5bd9501bce3573d53e83fb6d5b9cf183416f
Herbert Wolverson <herberticus@gmail.com>
2021-02-11 11:24:57 -0600

    #173 - Call resize on window move, to handle moving across displays with differing resolutions and scale factors.

--- bracket-terminal/src/hal/native/mainloop.rs
+++ bracket-terminal/src/hal/native/mainloop.rs
@@ -136,6 +136,12 @@ pub fn main_loop<GS: GameState>(mut bterm: BTerm, mut gamestate: GS) -> Result<(
                     bterm.on_event(BEvent::Moved {
                         new_position: Point::new(physical_position.x, physical_position.y),
                     });
+
+                    let scale_factor = wc.window().scale_factor();
+                    let physical_size = wc.window().inner_size();
+                    wc.resize(physical_size);
+                    on_resize(&mut bterm, physical_size, scale_factor, true)
+                        .unwrap();
                 }
                 WindowEvent::Resized(_physical_size) => {
                     let scale_factor = wc.window().scale_factor();
