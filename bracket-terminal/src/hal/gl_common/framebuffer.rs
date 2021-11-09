6e9557c3396e5da93fc3972d4df77a9a6469f3d0
Herbert Wolverson <herberticus@gmail.com>
2021-11-09 08:05:07 -0600

    #220 - Change the framebuffer texture scaling parameters. No artefacts when scaling on my system. Fullscreen/maximize still needs some love.

--- bracket-terminal/src/hal/gl_common/framebuffer.rs
+++ bracket-terminal/src/hal/gl_common/framebuffer.rs
@@ -40,12 +40,12 @@ impl Framebuffer {
             gl.tex_parameter_i32(
                 glow::TEXTURE_2D,
                 glow::TEXTURE_MIN_FILTER,
-                glow::LINEAR as i32,
+                glow::NEAREST as i32,
             );
             gl.tex_parameter_i32(
                 glow::TEXTURE_2D,
                 glow::TEXTURE_MAG_FILTER,
-                glow::LINEAR as i32,
+                glow::NEAREST as i32,
             );
             gl.tex_parameter_i32(
                 glow::TEXTURE_2D,
