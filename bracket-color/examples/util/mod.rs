3637346c1ab5826e069664772be95e46715ae529
Funnisimo <scastette@yahoo.com>
2022-08-23 10:01:13 -0500

    export colors mods separately to aid precise imports

--- bracket-color/examples/util/mod.rs
+++ bracket-color/examples/util/mod.rs
@@ -2,7 +2,7 @@ use bracket_color::prelude::*;
 use crossterm::queue;
 use crossterm::style::{Print, SetForegroundColor};
 use std::convert::TryInto;
-use std::io::{stdout, Write};
+use std::io::stdout;
 
 pub fn print_color(color: RGB, text: &str) {
     queue!(stdout(), SetForegroundColor(color.try_into().unwrap())).expect("Command Fail");
