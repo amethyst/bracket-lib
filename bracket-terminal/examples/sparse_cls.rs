9af83fd449ce456c135ddff9b7ec41215c701707
Jeremiah Russell <jerry@jrussell.ie>
2024-01-05 07:04:42 +0000

    refactor: Make code in sparse_cls idiomatic to remove warnings
    - replace match on Some only with if let Some()

--- bracket-terminal/examples/sparse_cls.rs
+++ bracket-terminal/examples/sparse_cls.rs
@@ -34,20 +34,17 @@ impl GameState for Demo {
             ctx.print_centered(0, "Sparse console");
         }
 
-        match ctx.key {
-            Some(VirtualKeyCode::Space) => {
-                if self.show_outer_console {
-                    ctx.set_active_console(1);
-                    ctx.cls();
+        if let Some(VirtualKeyCode::Space) = ctx.key {
+            if self.show_outer_console {
+                ctx.set_active_console(1);
+                ctx.cls();
 
-                    if USE_WORKAROUND {
-                        let transparent = RGBA::from_f32(0., 0., 0., 0.);
-                        ctx.set(40, 0, transparent, transparent, ' ' as u16);
-                    }
+                if USE_WORKAROUND {
+                    let transparent = RGBA::from_f32(0., 0., 0., 0.);
+                    ctx.set(40, 0, transparent, transparent, ' ' as u16);
                 }
-                self.show_outer_console = !self.show_outer_console
             }
-            _ => (),
+            self.show_outer_console = !self.show_outer_console
         }
     }
 }
